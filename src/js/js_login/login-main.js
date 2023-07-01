789bcbc990f95f51b0dbe9e56b8f89db7cd9f3a7
Javier Carranza <137971245+JavierCarranza2203@users.noreply.github.com>
2023-07-01 00:21:40 -0500

    Agregando variable de sesion

--- src/js/js_login/login-main.js
+++ src/js/js_login/login-main.js
@@ -1,7 +1,17 @@
-document.getElementById("btnAyuda").addEventListener("click", function(){
-    document.getElementById("help-container").classList.add("mostrar");
-});
+const inputs = document.querySelectorAll( '.form__field input' );
 
-document.getElementById("help-container__close-btn").addEventListener("click", function(){
-    document.getElementById("help-container").classList.remove("mostrar");
-});
+inputs.forEach( input => {
+	//cuando entramos en el input 
+	input.onfocus = ( ) => {
+		//al elemento anterior (el span) le agregamos la clase que la reubica en top
+		input.previousElementSibling.classList.add( 'reubicar' );
+	}
+	
+	//cuando salimos del input
+	input.onblur = ( ) => {
+		//si no hay texto, le quitamos la clase reubicar, 
+		//para que se superponga con el input
+		if( input.value.trim( ).length == 0 )
+		input.previousElementSibling.classList.remove( 'reubicar' );
+	}
+} );
--- src/php/php_login/login-query.php
+++ src/php/php_login/login-query.php
@@ -1,23 +1,24 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "notaria104";
+    include '../conection.php';
 
-    $conexion = mysqli_connect($host, $user, $pass, $db);
-
-    if(!$conexion){
+    if(!$conection){
         die("No hay conexion: ".mysqli_connect_error());
     }
 
     $usuario = $_POST["txtUsuario"];
     $password = $_POST["txtPassword"];
 
-    $query = mysqli_query($conexion,"SELECT*FROM usuarios WHERE nombre = '".$usuario."' and password = '".$password."'");
+    $query = mysqli_query($conexion, "SELECT*FROM usuarios WHERE nombre = '".$usuario."' and password = '".$password."'");
+    
     $nr = mysqli_num_rows($query);
 
     if($nr == 1){
-        header("Location: ../../Main-Page/index.html");
+        
+        session_start();
+
+        $_SESSION['usuario'] = $usuario;
+
+        header("Location: ../../../Pagina principal/index.html");
     }
     else if($nr == 0){
         echo "No ingreso";
